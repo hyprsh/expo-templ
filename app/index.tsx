b5c21f0a512512a1d74ce7768d47e0ab8cc37f26
nd <nd@hypr.sh>
2024-09-28 06:58:17 +0200

    feat: add eslint

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
-import tw from 'twrnc'
+import tw from "twrnc";
 
 export default function Index() {
   return (
-    <View style={tw`flex-1 justify-center items-center`} >
-      <Text style={tw`text-blue-500 dark:text-red-500`}>Edit app/index.tsx to edit this screen.</Text>
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Text style={tw`text-blue-500 dark:text-red-500`}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
